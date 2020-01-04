88a4b5457feab1a940b50db6e2ba189072b40144
taiebchaabini <taiebchaabini@gmail.com>
2020-01-04 21:05:28 +0000

    New changes

--- monty.h
+++ monty.h
@@ -10,6 +10,10 @@
 char *getData(char *filename);
 void interpreter(char *data);
 /**
+*
+**/
+
+/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
