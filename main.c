d1a2223c27e2bc1e71e59d44006e6d1c4e0f0db1
taiebchaabini <taiebchaabini@gmail.com>
2020-01-05 22:35:12 +0000

    Betty fixes

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "monty.h"
+/**
+* main - The monty program interpreter for monty byte code files 
+* Return: 0 otherwise EXIT_FAILURE
+**/
 int main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		printf("USAGE: monty file\n");
-		return (EXIT_FAILURE);
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
 	interpreter(av[1]);
 	return (0);
